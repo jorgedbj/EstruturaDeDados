9dc999e0616c18797d18ea33224564bfa777c6c4
Jorge Junior <jorgedbj@gmail.com>
2017-09-01 10:54:30 -0300

    Update Ex_3_6.java

--- Ex_3_6.java
+++ Ex_3_6.java
@@ -1,4 +1,4 @@
-// Give an algorithm for finding the second-to-last node in a singly linked list in which the last node
+// R-3.6 Give an algorithm for finding the second-to-last node in a singly linked list in which the last node
 // is indicated by a null next reference.
 
 public E findSecondLast() {
