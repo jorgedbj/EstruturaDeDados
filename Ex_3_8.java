0ba443329814f150683a4df5fe3fc93c1c4cae70
Jorge Junior <jorgedbj@gmail.com>
2017-09-03 10:10:24 -0300

    Update Ex_3_8.java

--- Ex_3_8.java
+++ Ex_3_8.java
@@ -1,5 +1,5 @@
 // R-3.8 Describe a method for finding the middle node of a doubly linked list with header and trailer
-// sentinels by “link hopping,” and without relying on explicit knowledge of the size of the list.
+// sentinels by “link hopping” and without relying on explicit knowledge of the size of the list.
 // In the case of an even number of nodes, report the node slightly left of center as the “middle.”
 // What is the running time of this method?
 
