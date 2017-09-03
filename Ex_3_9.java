0d523d0df071eee87b9678e2a4daea2f70f84194
Jorge Junior <jorgedbj@gmail.com>
2017-09-03 10:18:34 -0300

    Create Ex_3_9.java

---
+++ Ex_3_9.java
@@ -0,0 +1,12 @@
+// R-3.9 Give an implementation of the size() method for the SingularlyLinkedList class, assuming
+// that we did not maintain size as an instance variable.
+
+public int getSize() {
+  int sizeList = 0;
+  Node<E> walk = head;
+  while (walk != null) {
+    sizeList++;
+    walk = walk.getNext();
+	}
+	return sizeList;
+}
